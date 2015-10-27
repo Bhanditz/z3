b5c2b0f83510e3f30d525909b535f2f4d9e6741c
David Bau <david.bau@gmail.com>
2015-10-27 14:01:26 -0400

    Some more api support.

--- src/api/nodejs/index.js
+++ src/api/nodejs/index.js
@@ -3,6 +3,7 @@ var core = require('./js/z3core');
 var consts = require('./js/z3consts');
 var types = require('./js/z3types');
 var assert = require('assert');
+var util = require('util');
 
 exports.enable_trace = enable_trace;
 function enable_trace(msg) {
@@ -113,4 +114,127 @@ function _get_ctx(ctx) {
 var sym = to_symbol('x');
 assert.equal(_symbol2js(_get_ctx(), to_symbol("xfoo")), "xfoo");
 assert.equal(_symbol2js(_get_ctx(), to_symbol(42)), "k!42");
+
+exports.set_param = set_param;
+function set_param(kw) {
+  for (var key in kw) {
+    var value = kw[key];
+    core.global_param_set(('' + key).toUpperCase(), '' + value)
+  }
+}
+
+exports.reset_params = reset_params;
+function reset_params() {
+  core.global_param_reset_all();
+}
+
+exports.get_param = get_param;
+function get_param(name) {
+  var result = ref.alloc(ref.types.CString);
+  if (core.global_param_get(('' + name), result)) {
+    return result.deref();
+  }
+  throw new types.Z3Exception("failed to retrieve value for " + name);
+}
+
+/* This test passes, but it's noisy.
+assert.throws(function() {
+  console.log('got', get_param('foobar'));
+}, types.Z3Exception);
+*/
+assert.equal(get_param('nlsat.reorder'), 'true');
+set_param({'nlsat.reorder': false});
+assert.equal(get_param('nlsat.reorder'), 'false');
+reset_params();
+assert.equal(get_param('nlsat.reorder'), 'true');
+
+// Mark objects that use pretty printer
+exports.Z3PPObject = Z3PPObject;
+function Z3PPObject() { }
+Z3PPObject.prototype.use_pp = function() { return true; }
+
+exports.AstRef = AstRef;
+function AstRef(ast, ctx) {
+  this.ast = ast;
+  this.ctx = _get_ctx(ctx);
+  core.inc_ref(this.ctx.ref(), this.as_ast());
+}
+util.inherits(AstRef, Z3PPObject);
+AstRef.prototype.del = function() {
+  core.dec_ref(this.ctx.ref(), this.as_ast());
+}
+/*
+AstRef.prototype.toString = function() {
+  printer.obj_to_string(this);
+}
+*/
+AstRef.prototype.sexpr = function() {
+  return core.ast_to_string(this.ctx_ref(), this.as_ast());
+}
+AstRef.prototype.as_ast = function() {
+  return this.ast;
+}
+AstRef.prototype.get_id = function() {
+  return core.get_ast_id(this.ctx_ref(), this.as_ast());
+}
+AstRef.prototype.ctx_ref = function() {
+  return this.ctx.ref();
+}
+AstRef.prototype.equals = function(other) {
+  _z3_assert(is_ast(other), "Z3 AST expected");
+  return core.is_eq_ast(this.ctx_ref(), this.as_ast(), other.as_ast());
+}
+AstRef.prototype.translate = function(target) {
+  _z3_assert(target instanceof Context, "argument must be a Z3 context");
+  return _to_ast_ref(core.translate(this.ctx_ref(), this.as_ast(), target.ref(), target));
+}
+AstRef.prototype.hash = function() {
+  return core.get_ast_hash(this.ctx_ref(), this.as_ast());
+}
+exports.is_ast = is_ast;
+function is_ast(a) {
+  return a instanceof AstRef;
+}
+exports.eq = eq;
+function eq(a, b) {
+  _z3_assert(is_ast(a) && is_ast(b), "Z3 ASTs expected");
+  return a.equals(b);
+}
+function _ast_kind(ctx, a) {
+  if (is_ast(a)) {
+    var a = a.as_ast();
+  }
+  return core.get_ast_kind(ctx.ref(), a);
+}
+function _ctx_from_ast_arg_list(args, default_ctx) {
+  var ctx = null;
+  for (var j = 0; j < args.length; ++j) {
+    var a = args[j];
+    if (is_ast(a) || is_probe(a)) {
+      if (ctx == null) {
+        ctx = a.ctx;
+      } else {
+        _z3_assert(ctx == a.ctx, "Context mimsatch");
+      }
+    }
+  }
+  return ctx || default_ctx || null;
+}
+function _ctx_from_ast_args() {
+  return _ctx_from_ast_arg_list(arguments);
+}
+function _to_func_decl_array(args) {
+  var fd_args = [];
+  for (var j = 0; j < args.length; ++j) {
+    fd_args.push(args[j].as_func_decl());
+  }
+  return fd_args;
+}
+function _to_ast_array(args) {
+  var ast_args = [];
+  for (var j = 0; j < args.length; ++j) {
+    ast_args.push(args[j].as_ast());
+  }
+  return ast_args;
+}
 
